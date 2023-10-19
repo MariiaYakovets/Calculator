857687cb6405cf5c76b42b5bc8f1189128953330
Mariia <yamaria05@gmail.com>
2023-10-19 20:48:52 +0100

    v4 Calculator
    Infinitely working calculator for as many operations as needed. All of them are followed by the previous ones and based on them

--- main.py/Calculator.py
+++ main.py/Calculator.py
@@ -7,38 +7,45 @@ print("would you like to count?")
 respond = input()
 result = None
 
-while respond == "yes":
-    value1 = int(input ("Type the value" "\n"))
-    operation1 = input ("choose your operation: " "\n" "+" "\n" "-" "\n" "*" "\n" "/" "\n" "**")
-    value2 = int(input ("Type the value" "\n"))
-    operation2 = input ("choose your operation: " "\n" "+" "\n" "-" "\n" "*" "\n" "/" "\n" "**" "\n" "=" )
-    if operation1 == "+":
-            result = value1 + value2
-    elif operation1 == "-":
-        result = value1 - value2
-    elif operation1 == "*":
-        result = value1 * value2
-    elif operation1 == "**":
-        result = value1 ** value2
-    elif operation1 == "/":
-        while value2!=0:
-            result = value1 / value2
+def sum ( a = None, b = None):
+    return a + b
+def dif (a = None, b = None):
+    return a - b
+def mult (a = None, b = None):
+    return a * b
+def div (a = None, b = None):
+    return a / b
+def pow (a = None, b = None):
+    return a ** b
+def calc (v1 = None, v2 = None, act = None):
+    if act == "+":
+        result = sum(a=v1, b=v2)
+    elif act == "-":
+        result = dif(a=v1, b=v2)
+    elif act == "*":
+        result = mult(a=v1, b=v2)
+    elif act == "**":
+        result = pow(a=v1, b=v2)
+    elif act == "/":
+        while v2 != 0:
+            result = div(a=v1, b=v2)
+            break
         else:
-            print (" devision by 0 is impossible, try something different")
-            
-    value3 = int(input ("Type the value" "\n"))
-    operation3 = input ("choose your operation: " "\n" "+" "\n" "-" "\n" "*" "\n" "/" "\n" "**" "\n" "=" )
-    if operation3 == "=":
-        if operation1 == "+":
-            result = value1 + value2
-        elif operation1 == "-":
-            result = value1 - value2
-        elif operation1 == "*":
-            result = value1 * value2
-        elif operation1 == "**":
-            result = value1 ** value2
-        elif operation1 == "/":
-            while value2!=0:
-                result = value1 / value2
-            else:
-                print (" devision by 0 is impossible, try something different")
+            print("division by 0 is impossible")
+            v2 = float (input("type your value" "\n"))
+    return result
+while respond == "yes":
+    v1 = float (input("type your value" "\n"))
+    result = v1
+    act1 = ""
+    while act1 != "=":
+        act1 = input ("Type operation" "\n")
+        if act1 != "=":
+            v2 = float (input("type your value" "\n"))
+            result = calc (result, v2, act1)
+        
+    else:
+        print("Your value is:", result)
+        respond = input ("Do you want to continue counting?" "\n")
+else:
+    print ("See you later then")
